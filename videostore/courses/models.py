b38bce438914f75af40356335ad21ec9ac049697
hammer <ttkagentusolie@gmail.com>
2024-05-09 02:31:46 +0800

    Вывел видео на отдельной странице урока с описанием

--- videostore/courses/models.py
+++ videostore/courses/models.py
@@ -17,3 +17,22 @@ class Course(models.Model):
 
     def get_absolute_url(self):
         return reverse('course-detail', kwargs={'slug': self.slug})
+
+
+class Lesson(models.Model):
+    slug = models.SlugField('Имя урока на латинице')
+    title = models.CharField('Название урока', max_length=120)
+    desc = models.TextField('Описание урока')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Какой курс?')
+    number = models.IntegerField('Номер урока')
+    video = models.CharField('Ссылка на видео', max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def get_absolute_url(self):
+        return reverse('lesson-detail', kwargs={'slug': self.course.slug, 'lesson_slug': self.slug})
