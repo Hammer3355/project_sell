ddf595ac94fe5c492abd594daf2dd54468eeccd7
hammer <ttkagentusolie@gmail.com>
2024-05-08 16:50:17 +0800

    Вывод курсов на главной странице. Шапка. Стили

--- videostore/courses/urls.py
+++ videostore/courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-
+    path('', views.HomePage.as_view(), name='home'),
 ]
 
 
--- videostore/courses/views.py
+++ videostore/courses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
-# Create your views here.
+
+class HomePage(ListView):
+    model = Course
+    template_name = 'courses/home.html'
+    context_object_name = 'courses'
+    ordering = ['-id']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        ctx = super(HomePage, self).get_context_data(**kwargs)
+        ctx['title'] = 'Главная страница'
+        return ctx
